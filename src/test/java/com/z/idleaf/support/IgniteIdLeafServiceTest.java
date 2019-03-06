f10b94ed5439be6a570cbe5da162c69665b789c8
王磊 <wanglei@jingdata.com>
2019-03-06 14:58:49 +0800

    init git ignore

--- src/test/java/com/z/idleaf/support/IgniteIdLeafServiceTest.java
+++ src/test/java/com/z/idleaf/support/IgniteIdLeafServiceTest.java
@@ -1,27 +1,27 @@
-/**
- * 
- */
-package com.z.idleaf.support;
-
-import org.junit.Test;
-
-import com.z.idleaf.support.IgniteIdLeafServiceImpl;
-
-/**
- * @author sunff
- *
- */
-public class IgniteIdLeafServiceTest {
-
-	@Test
-	public void getId() {
-
-		IgniteIdLeafServiceImpl idLeaf = new IgniteIdLeafServiceImpl();
-		idLeaf.setBizTag("order");
-		idLeaf.setZkAddress("localhost:2181");
-		idLeaf.init();
-		while (true) {
-			System.out.println(idLeaf.getId());
-		}
-	}
-}
+///**
+// *
+// */
+//package com.z.idleaf.support;
+//
+//import org.junit.Test;
+//
+//import com.z.idleaf.support.IgniteIdLeafServiceImpl;
+//
+///**
+// * @author sunff
+// *
+// */
+//public class IgniteIdLeafServiceTest {
+//
+//	@Test
+//	public void getId() {
+//
+//		IgniteIdLeafServiceImpl idLeaf = new IgniteIdLeafServiceImpl();
+//		idLeaf.setBizTag("order");
+//		idLeaf.setZkAddress("localhost:2181");
+//		idLeaf.init();
+//		while (true) {
+//			System.out.println(idLeaf.getId());
+//		}
+//	}
+//}
